9b8a1fff2da193123cf35628f976fb1961d698c4
Hugo Marinho <hugoharan@gmail.com>
2019-11-11 15:55:11 -0300

    Updates

---
+++ precache-manifest.fa9fc8aa1f993ae8f92a31c1814deae4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "907396be3e2f05db2ab3e088456e63f6",
+    "url": "/faladilma/index.html"
+  },
+  {
+    "revision": "d466798fcde05d2051da",
+    "url": "/faladilma/static/css/main.7f6952e5.chunk.css"
+  },
+  {
+    "revision": "0ef3bb35d01627618bb3",
+    "url": "/faladilma/static/js/2.13559619.chunk.js"
+  },
+  {
+    "revision": "d466798fcde05d2051da",
+    "url": "/faladilma/static/js/main.9526f1c6.chunk.js"
+  },
+  {
+    "revision": "ad78003495393498326d",
+    "url": "/faladilma/static/js/runtime-main.ccd8efc4.js"
+  },
+  {
+    "revision": "2751921e1d6c2b0c7d5f8c3bbae18b2b",
+    "url": "/faladilma/static/media/mamae-dilma.2751921e.png"
+  }
+]);
